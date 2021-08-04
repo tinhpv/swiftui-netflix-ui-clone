36e28ee9cab5a93e8edf4fa684cc31bc6b27cfd6
tinhpv <vutinh.phan@gmail.com>
2021-08-04 22:31:25 +0700

    Initial Commit

---
+++ Netflix Rebuild/Netflix_RebuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_RebuildApp.swift
+//  Netflix Rebuild
+//
+//  Created by TinhPV on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_RebuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
